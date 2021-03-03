5eb2a62ddc51b79e3a8db9818eab3f9efe09da7c
Tomáš Hapl <tomas.hapl@oriflame.com>
2021-03-03 15:46:51 +0100

    auto reconnection.

--- service.lili_control/main.py
+++ service.lili_control/main.py
@@ -1,13 +1,32 @@
 import os
+from functools import partial
 import json
-import xbmc
+import time
 import ssl
+import xbmc
 import websocket
 
 
+def execute_build_in(ws, params):
+    print("execute_build_in", params)
+    xbmc.executebuiltin(params)
+
+
+def get_playing_stream(ws):
+    print("get stream")
+    current_stream = xbmc.Player().getPlayingFile()
+    ws.send(json.dumps({"stream": current_stream}))
+
+
 def on_message(ws, message):
-    print(message)
-    xbmc.executebuiltin(message)
+    command_message = json.loads(message)
+    switcher = {
+        "execute_build_in": partial(execute_build_in, ws, command_message["params"]),
+        "get_playing_stream": partial(get_playing_stream, ws),
+    }
+
+    call = switcher.get(command_message["command"], lambda: "Invalid command")
+    call()
 
 
 def on_error(ws, error):
@@ -19,27 +38,31 @@ def on_close(ws):
 
 
 def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            ws.send("Hello %d" % i)
-        ws.close()
-        print("thread terminating...")
+    print("Opened")
+
+
+def connect():
+    while True:
+        try:
+            secrets = {
+                "secret": "EE71C236BEC72A259BACAB36562FC",
+                "id": os.uname()[1],
+            }
+            websocket.enableTrace(True)
+            ws = websocket.WebSocketApp(
+                "ws://localhost:8080",
+                on_open=on_open,
+                on_message=on_message,
+                on_error=on_error,
+                on_close=on_close,
+                header=["authorization: {0}".format(json.dumps(secrets))],
+            )
+
+            ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+        except Exception as e:
+            print("Websocket connection Error  : {0}".format(e))
+            time.sleep(5)
 
 
 if __name__ == "__main__":
-
-    secrets = {
-        "secret": "EE71C236BEC72A259BACAB36562FC",
-        "id": xbmc.getInfoLabel("System.FriendlyName") or os.uname()[1],
-    }
-
-    websocket.enableTrace(True)
-    ws = websocket.WebSocketApp(
-        "wss://lili.psvz.cz/websockets",
-        on_message=on_message,
-        on_error=on_error,
-        on_close=on_close,
-        header=["authorization: {0}".format(json.dumps(secrets))],
-    )
-    ws.on_open = on_open
-    ws.run_forever(sslopt={"cert_reqs": ssl.CERT_NONE})
+    connect()
